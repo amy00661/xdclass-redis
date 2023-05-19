01352809b58378ea25882d0fef01a6f5f2c2139d
Amy Chung <amy00661@gmail.com>
2023-05-19 14:18:51 +0800

    [第十章][第2集][配置]SpringBoot2.x整合MybatisPlus連接Mysql數據庫

---
+++ src/main/java/net/xdclass/xdclassredis/model/ProductDO.java
@@ -0,0 +1,100 @@
+package net.xdclass.xdclassredis.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.util.Date;
+
+@TableName("product")
+public class ProductDO {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 標題
+     */
+    private String title;
+
+    /**
+     * 封面圖
+     */
+    private String coverImg;
+
+    /**
+     * 詳情
+     */
+    private String detail;
+
+    /**
+     * 新價格
+     */
+    private Integer amount;
+
+    /**
+     * 庫存
+     */
+    private Integer stock;
+
+    /**
+     * 創建時間
+     */
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCoverImg() {
+        return coverImg;
+    }
+
+    public void setCoverImg(String coverImg) {
+        this.coverImg = coverImg;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
